5ea951e3a7c92fab1a78ccfaf0051d6f6535c705
Lexseal Lin <zhongjiezhex12@me.com>
2020-12-18 00:16:00 -0800

    working prototype

--- main.cpp
+++ main.cpp
@@ -2,13 +2,46 @@
 #include <iostream>
 #include <complex>
 
-#define WIDTH 1000
-#define HEIGHT 1000
+#define X_MIN -1.5
+#define X_MAX 0.5
+#define Y_MIN -1
+#define Y_MAX 1
 
-#define ITER 10
+#define RESOLUTION 500
+
+#define ITER 50
+
+sf::Color hsv(int hue, float sat, float val) {
+    hue %= 360;
+    while (hue<0) hue += 360;
+
+    if (sat<0.f) sat = 0.f;
+    if (sat>1.f) sat = 1.f;
+
+    if (val<0.f) val = 0.f;
+    if (val>1.f) val = 1.f;
+
+    int h = hue / 60;
+    float f = float(hue) / 60 - h;
+    float p = val*(1.f - sat);
+    float q = val*(1.f - sat*f);
+    float t = val*(1.f - sat*(1 - f));
+
+    switch (h) {
+		default:
+		case 0:
+		case 6: return sf::Color(val * 255, t * 255, p * 255);
+		case 1: return sf::Color(q * 255, val * 255, p * 255);
+		case 2: return sf::Color(p * 255, val * 255, t * 255);
+		case 3: return sf::Color(p * 255, q * 255, val * 255);
+		case 4: return sf::Color(t * 255, p * 255, val * 255);
+		case 5: return sf::Color(val * 255, p * 255, q * 255);
+    }
+}
 
 void set(sf::Uint8* pixels, const int &r, const int &c, const sf::Color &color) {
-	sf::Uint8 *pixel = pixels+(r*WIDTH+c)*4;
+	int width = (X_MAX-X_MIN)*RESOLUTION;
+	sf::Uint8 *pixel = pixels+(r*width+c)*4;
 	*pixel = color.r; pixel++;
 	*pixel = color.g; pixel++;
 	*pixel = color.b; pixel++;
@@ -16,44 +49,44 @@ void set(sf::Uint8* pixels, const int &r, const int &c, const sf::Color &color) 
 }
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "mandelbrot");
-	sf::Uint8 *pixels = new sf::Uint8[WIDTH * HEIGHT * 4];
+	int width = (X_MAX-X_MIN)*RESOLUTION;
+	int height = (Y_MAX-Y_MIN)*RESOLUTION;
+	sf::RenderWindow window(sf::VideoMode(width, height), "mandelbrot");
+	sf::Uint8 *pixels = new sf::Uint8[width * height * 4];
 	sf::Texture texture;
-	texture.create(WIDTH, HEIGHT);
+	texture.create(width, height);
 	sf::Sprite sprite;
 	sprite.setTexture(texture);
 
+	int count = 0;
 	while (window.isOpen()) {
 		sf::Event event;
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed) window.close();
 		}
 
-		for (int r = 0; r < HEIGHT; r++) {
-			for (int c = 0; c < WIDTH; c++) {
-				set(pixels, r, c, sf::Color((r%(c+1))%255, (r*(c+1))%255, (r+c)%255, 255));
-			}
-		}
-		for (int a = -WIDTH/2; a < WIDTH/2; a++) {
-			for (int b = -HEIGHT/2; b < HEIGHT/2; b++) {
-				std::complex<double> z(0., 0.);
-				std::complex<double> c(double(a)/500., double(b)/500.);
-				for (int i = 0; i < ITER; i++) {
-					z = pow(z, 2) + c;
-				}
+		for (int a = X_MIN*RESOLUTION; a < X_MAX*RESOLUTION; a++) {
+			for (int b = Y_MIN*RESOLUTION; b < Y_MAX*RESOLUTION; b++) {
+				std::complex<double> c(double(a)/double(RESOLUTION), double(b)/double(RESOLUTION));
+				std::complex<double> z = c;
 				double length = norm(z);
-				std::cout << length << std::endl;
+				for (int i = 0; i < ITER && length < 2; i++) {
+					z = pow(z, 2) + c;
+					length = norm(z);
+				}
 				
-				if (length > 20) {
-					set(pixels, b+HEIGHT/2, a+WIDTH/2, sf::Color(int(length)%255, 
-																 int(length)%255, 
-																 int(length)%255, 
-																 255));
+				//std::cout << length << std::endl;
+				
+				int row = b-Y_MIN*RESOLUTION;
+				int col = a-X_MIN*RESOLUTION;
+				if (length > 2) {
+					set(pixels, row, col, sf::Color::Black);
 				} else {
-					set(pixels, b+HEIGHT/2, a+WIDTH/2, sf::Color(10, 0, 0, 255));
+					set(pixels, row, col, hsv(length*180, 1, 1));
 				}
 			}
 		}
+		std::cout << "loop " << count++ << std::endl;
 
 		texture.update(pixels);
 
